82df1a54e8b8a760a6225f8ecc5b0ae52f3618dd
eshtel <estelle.pignon@unil.ch>
2025-01-29 20:06:07 +0000

    added pictures
    and added about picture

--- MicroscopyWebsite/scripts.js
+++
@@ -1,6 +0,0 @@
-// Example: alert when an image is clicked
-document.querySelectorAll('.gallery img').forEach((img) => {
-    img.addEventListener('click', () => {
-        alert('You clicked on ' + img.alt);
-    });
-});
